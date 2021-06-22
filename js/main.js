acb33c715a60e060eec129b0cdef40fbcd028ad4
Giovane-Roberti-Tafine <kstv23@hotmail.com>
2021-06-22 17:44:26 -0300

    Inserindo novo serviço e retirando moment

--- js/main.js
+++ js/main.js
@@ -18,9 +18,9 @@ window.addEventListener('scroll', (event) => {
 window.addEventListener('resize', () => {
     if (screen.width <= 768) {
         nav.classList.add('scroll');
-        console.log('scroll');
+        // console.log('scroll');
     } else {
-        console.log('width');
+        // console.log('width');
         window.scrollY > 44 ? nav.classList.add('scroll') : nav.classList.remove('scroll');
     }
 
@@ -36,17 +36,13 @@ window.addEventListener('resize', () => {
 
 window.addEventListener('click', (event) => {
     if (event.path.indexOf(document.querySelector('.box-time')) < 0 && screen.width < 970) {
-        console.log(event.path.indexOf(document.querySelector('.box-time')));
+        // console.log(event.path.indexOf(document.querySelector('.box-time')));
         $('.schedule').css('width', '0px');
         $('.schedule').css('opacity', '0');
     }
 });
 
 $(document).ready(function () {
-    var data = new Date()
-
-    data.toLocaleTimeString('pt-br')
-
     if (screen.width < 768) {
         nav.classList.add('scroll');
     } else {
@@ -67,52 +63,52 @@ $(document).ready(function () {
     );
 
     // Enviar email
-    $('#btn').click( function(){
+    $('#btn').click(function () {
         /* Coletando dados */
-        
-        if($('#nome').val().trim() === '' || $('#nome').val().trim().length < 5) {
-            $('#nome').focus()
-            return
-        } 
-        var patt1 = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/; 
-        if($('#email').val().trim() === '' || !($('#email').val().trim().match(patt1))) {
-            $('#email').focus()
-            return
-        } 
-        if($('#assunto').val().trim() === '' || $('#assunto').val().trim().length < 5) {
-            $('#assunto').focus()
-            return
-        } 
-        if($('#mensagem').val().trim() === '' || $('#mensagem').val().trim().length < 10) {
-            $('#mensagem').focus()
-            return
-        } 
+
+        if ($('#nome').val().trim() === '' || $('#nome').val().trim().length < 5) {
+            $('#nome').focus();
+            return;
+        }
+        var patt1 = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if ($('#email').val().trim() === '' || !($('#email').val().trim().match(patt1))) {
+            $('#email').focus();
+            return;
+        }
+        if ($('#assunto').val().trim() === '' || $('#assunto').val().trim().length < 5) {
+            $('#assunto').focus();
+            return;
+        }
+        if ($('#mensagem').val().trim() === '' || $('#mensagem').val().trim().length < 10) {
+            $('#mensagem').focus();
+            return;
+        }
 
         var urlData = $('#formExemplo').serialize();
 
         $.ajax({
-            type : 'POST',
-            url  : './js/email.php',
-            data : urlData,
+            type: 'POST',
+            url: './js/email.php',
+            data: urlData,
             dataType: 'json',
             success: (result) => {
                 if (result.status === '200') {
-                    $('#resposta-email').html(result.mensagem).show()
+                    $('#resposta-email').html(result.mensagem).show();
                     setTimeout(() => {
-                        $('#resposta-email').hide()
-                    }, 10000)
+                        $('#resposta-email').hide();
+                    }, 10000);
                 } else if (result.status === '400') {
-                    $('#resposta-email').html(result.mensagem).css('color', 'red').show()
+                    $('#resposta-email').html(result.mensagem).css('color', 'red').show();
                     setTimeout(() => {
-                        $('#resposta-email').hide()
-                    }, 10000)
-                } 
+                        $('#resposta-email').hide();
+                    }, 10000);
+                }
             },
             error: (result) => {
-                $('#resposta-email').html('Infelizmente houve um erro ao enviar sua mensagem!').css('color', 'red').show()
+                $('#resposta-email').html('Infelizmente houve um erro ao enviar sua mensagem!').css('color', 'red').show();
                 setTimeout(() => {
-                    $('#resposta-email').hide()
-                }, 10000)
+                    $('#resposta-email').hide();
+                }, 10000);
 
             },
             beforeSend: () => { /* antes de enviar */
@@ -122,9 +118,9 @@ $(document).ready(function () {
                 $('#loading').fadeOut('fast'); /* esconde o loading */
                 $('#formExemplo')[0].reset();
             }
-            
+
         });
-        
+
         temp = null;
 
         return false;
@@ -133,8 +129,8 @@ $(document).ready(function () {
 
     // Link desenvolvido
     $('#desenvolvido').click(() => {
-        window.open('https://api.whatsapp.com/send?phone=5511968397621', '_blank')
-    })
+        window.open('https://api.whatsapp.com/send?phone=5511968397621', '_blank');
+    });
 });
 
 
@@ -155,10 +151,21 @@ window.addEventListener('scroll', (event) => {
 });
 
 var horas = setInterval(() => {
-    moment.locale('pt');
-    $('#new-york').text(moment().subtract('01:00:00').format('LT'));
-    $('#pequim').text(moment().add('11:00:00').format('LT'));
-    $('#londres').text(moment().add('04:00:00').format('LT'));
+    const data = new Date();
+
+    $('#new-york').text(data.toLocaleString("pt-br", {
+        timeZone: "America/New_York", hour: '2-digit',
+        minute: '2-digit', formatMatcher: "basic"
+    }));
+    $('#londres').text(data.toLocaleString("pt-br", {
+        timeZone: "Europe/London", hour: '2-digit',
+        minute: '2-digit', formatMatcher: "basic"
+    }));
+    $('#pequim').text(data.toLocaleString("pt-br", {
+        timeZone: "Asia/Hong_Kong", hour: '2-digit',
+        minute: '2-digit', formatMatcher: "basic"
+    }));
+
 }, 1000);
 
 function openNav() {
